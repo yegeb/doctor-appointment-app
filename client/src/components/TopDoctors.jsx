74dba293528fae9ae96e1536a269729347f5407c
yegeb <yukselege35@gmail.com>
2025-07-10 15:01:52 +0300

    bugfix in TopDoctors.jsx

--- client/src/components/TopDoctors.jsx
+++ client/src/components/TopDoctors.jsx
@@ -18,7 +18,7 @@ const TopDoctors = () => {
         </p>
         <div className='w-full grid grid-cols-auto-fill-200 gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
             {doctors.slice(0, 10).map((item, index)=>(
-                <div onClick={() => navigate(` /appointment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index} >
+                <div onClick={() => {navigate(`/appointment/${item._id}`); scrollTo(0, 0)}} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index} >
                     <img className='bg-blue-50' src={item.image} alt="" />
                     <div className='p-4'>
                         <div className='flex items-center gap-2 text-sm text-center text-green-500'>
