106cd41109a7395ee252ee47c8b20c8ec55ecafa
yegeb <yukselege35@gmail.com>
2025-07-12 11:03:50 +0300

    server initial commit

---
+++ server/controllers/adminController.js
@@ -0,0 +1,15 @@
+
+
+// API for adding doctor
+const addDoctor = async(req, res) => {
+
+    try{
+        const { name, email, password, speciality, degree, experience, about, fee, address } = req.body
+    } 
+    catch(error){
+
+    }
+
+}
+
+export {addDoctor}
