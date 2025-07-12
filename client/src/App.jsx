5ad0c695e7a4b4ddf42b786c8b03137d529c191a
yegeb <yukselege35@gmail.com>
2025-07-12 11:03:13 +0300

    add small changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
         <Route path='/contact' element={<Contact/>} />
         <Route path='/my-profile' element={<MyProfile/>} />
         <Route path='/my-appointments' element={<MyAppointments/>} />
-        <Route path='/appointment/:docID' element={<Appointment/>} />
+        <Route path='/appointment/:docId' element={<Appointment/>} />
       </Routes>
       <Footer/>
 
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -4,8 +4,12 @@ import { doctors } from "../assets/assets";
 export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
+
+    const currencySymbol = '$'
+
     const value = {
-        doctors
+        doctors, 
+        currencySymbol
     }
     return(
         <AppContext.Provider value={value}>
