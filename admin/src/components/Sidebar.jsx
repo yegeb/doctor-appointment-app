92c01d7f30954ba2a64e5b39b3558f0ade9cdd49
yegeb <yukselege35@gmail.com>
2025-07-31 11:49:55 +0300

    add Sidebar.jsx

---
+++ admin/src/components/Sidebar.jsx
@@ -0,0 +1,50 @@
+import React, { useContext } from 'react'
+import { AdminContext } from '../context/AdminContext'
+import { NavLink } from 'react-router-dom'
+{ /* import { assets } from '../assets/assets' ASSETS FOLDER IS MISSING */ }
+
+const Sidebar = () => {
+
+    const {aToken} = useContext(AdminContext)
+
+  return (
+    <div className='min-h-screen bg-white border-r'>
+        {
+            aToken && <ul className='text-[#515151] mt-5'>
+            
+            <li>
+            <NavLink className={({isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`} to={'/admin-dashboard'}>
+                {/* <img src={assets.home_icon} alt="" /> */}  
+                <p>Dashboard</p>
+            </NavLink>
+            </li>
+
+            <li>
+            <NavLink className={({isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4  border-primary' : ''}`} to={'/all-appointments'}>
+                {/* <img src={assets.appointment_icon} alt="" /> */}   
+                <p>Appointments</p>
+            </NavLink>
+            </li>
+
+            <li>
+            <NavLink className={({isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4  border-primary' : ''}`} to={'/add-doctor'}>
+                {/* <img src={assets.add_icon} alt="" /> */}   
+                <p>Add Doctor</p>
+            </NavLink>
+            </li>
+
+            <li>
+            <NavLink className={({isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4  border-primary' : ''}`} to={'/doctor-list'}>
+                {/* <img src={assets.people_icon} alt="" /> */}   
+                <p>Doctors List</p>
+            </NavLink> 
+            </li>
+
+            </ul>
+        }
+    </div>
+  )
+}
+
+export default Sidebar
+
