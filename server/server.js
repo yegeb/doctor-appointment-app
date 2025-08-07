51e85791bbb8f8d8ad6bafdd9f3bf59a0d67cb60
yegeb <yukselege35@gmail.com>
2025-08-07 09:20:53 +0300

    small update

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import adminRouter from './routes/adminRoute.js'
+import doctorRouter from './routes/doctorRoute.js'
+import userRouter from './routes/userRoute.js'
 
 // app config 
 const app = express()
@@ -16,8 +18,17 @@ connectCloudinary()
 app.use(express.json())
 app.use(cors())
 
+// Debugging middleware to see requests
+app.use((req, res, next) => {
+  console.log(`[${req.method}] ${req.url}`);
+  console.log('Headers:', req.headers);
+  next();
+});
+
 // api endpoints
 app.use('/api/admin', adminRouter)
+app.use('/api/doctor', doctorRouter)
+app.use('/api/user', userRouter)
 
 app.get('/', (req, res) => {
     res.send('API WORKING')
