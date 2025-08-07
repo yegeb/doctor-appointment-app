208e2e0719365cd4198444754780a07dd301543a
yegeb <yukselege35@gmail.com>
2025-08-07 09:19:42 +0300

    update controllers for functional appointment system

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,8 +1,10 @@
 import validator from 'validator';
-import bycrypt from 'bcrypt';
+import bcrypt from 'bcrypt';
 import { v2 as cloudinary } from 'cloudinary'
 import doctorModel from '../models/doctorModel.js'
 import jwt from 'jsonwebtoken'
+import appointmentModel from '../models/appointmentModel.js';
+import userModel from '../models/userModel.js';
 
 // API for adding doctor
 const addDoctor = async(req, res) => {
@@ -27,25 +29,25 @@ const addDoctor = async(req, res) => {
             return res.status(400).json({success:false, message: "Invalid email format"});
         }
 
-        // Validate strong password
+        // [NOTE]: CHANGE THE PASSWORD SECURITY CRITERIA !!!
         if(password.length < 8) {
             return res.status(400).json({success:false, message: "Password must be at least 8 characters long"});
         }
 
         // Hash the doctor password
-        const salt = await bycrypt.genSalt(10);
-        const hashedPassword = await bycrypt.hash(password, salt);
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
         // Upload image to cloudinary
         const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type:"image"});
         const imageURL = imageUpload.secure_url
 
 
-        const doctorData = {
+        const docData = {
             name,
             email,
-            image:imageURL,
-            password:hashedPassword,
+            image: imageURL,
+            password: hashedPassword,
             speciality,
             degree,
             experience,
@@ -55,7 +57,7 @@ const addDoctor = async(req, res) => {
             date:Date.now()
         }
 
-        const newDoctor = new doctorModel(doctorData);
+        const newDoctor = new doctorModel(docData);
         await newDoctor.save();
 
         res.json({success:true, message:" Doctor Added"});
@@ -84,10 +86,99 @@ const loginAdmin = async (req, res) => {
       return res.status(400).json({ success: false, message: "Invalid credentials" });
     }
 
-  } catch (error) {
+  } 
+  
+  catch (error) {
     console.error("[ERROR] Admin login failed:", error);
     return res.status(500).json({ success: false, message: error.message });
   }
 }
 
-export { addDoctor, loginAdmin };
+// API to get all doctors list for admin panel
+const allDoctors = async (req, res) => {
+  try{
+    const doctors = await doctorModel.find({}).select('-password')
+    res.json({success: true, doctors})
+  }
+
+  catch (error) {
+    console.error(error);
+    return res.status(500).json({ success: false, message: error.message });
+  }
+}
+
+// API to get all appointments list
+const appointmentAdmin = async (req, res) => {
+  try {
+    const appointments = await appointmentModel.find({})
+    res.json({success: true, appointments})
+  } 
+  
+  catch (error) {
+    console.error(error);
+    return res.status(500).json({ success: false, message: error.message });
+  }
+}
+
+// API to cancel appointments
+const adminCancelAppointment = async (req, res) => {
+    try {
+        const {userId, appointmentId} = req.body
+
+        const appointmentData = await appointmentModel.findById(appointmentId)
+
+        // Verify user for the appointment
+        if (appointmentData.userId.toString() !== userId.toString()) {
+          return res.json({ success: false, message: 'Unauthorized action' })
+        }
+
+        await appointmentModel.findByIdAndUpdate(appointmentId, {cancelled: true})
+
+        // Releasing doctor slot
+
+        const { docId, slotDate, slotTime } = appointmentData
+
+        const docData = await doctorModel.findById(docId)
+
+        let slots_booked = docData.slots_booked
+
+        if (slots_booked[slotDate]) {
+          slots_booked[slotDate] = slots_booked[slotDate].filter(e => e !== slotTime)
+        }
+
+        await doctorModel.findByIdAndUpdate(docId, {slots_booked})
+
+        res.json({success: true, message: "Appointment has been cancelled"})
+    } 
+    
+    catch (error) {
+      console.log(error)
+      res.json({success: false, message: error.message})  
+    }
+}
+
+// API to get dashboard data for admin panel
+const adminDash = async (req, res) => {
+  try {
+    const doctors = await doctorModel.find({})
+    const users = await userModel.find({})
+    const appointments = await appointmentModel.find({})
+
+    const dashData = {
+      doctors           : doctors.length,
+      appointments      : appointments.length,
+      patients          : users.length,
+      latestAppointments: [...appointments].reverse().slice(0, 5)
+    }
+
+    res.json({success: true, dashData})
+  } 
+  
+  catch (error) {
+    console.log(error)
+    res.json({success: false, message: error.message}) 
+  }
+}
+
+
+export { addDoctor, loginAdmin, allDoctors, appointmentAdmin, adminCancelAppointment, adminDash };
