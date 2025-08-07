96dec8da56a6eaf55ef77c74329d97f22a4b6587
yegeb <yukselege35@gmail.com>
2025-08-07 09:18:43 +0300

    update multer.js fix cloudinary image system

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -1,6 +1,9 @@
 import multer from 'multer'
 
 const storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, 'uploads/') // or any folder you want
+    },
     filename: function(req, file, callback){
         callback(null, file.originalname)
     }
